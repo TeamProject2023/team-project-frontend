1a85f091b4fc4b1619979267f28c43d6bdbe9aed
Natallia Kostsina <natakostina123@gmail.com>
2023-12-09 10:18:33 +0100

    feat: delete header and footer in auth pages

--- src/layouts/AuthLayout/AuthLayout.tsx
+++ src/layouts/AuthLayout/AuthLayout.tsx
@@ -4,11 +4,9 @@ import { Outlet } from "react-router-dom";
 export const AuthLayout: FC = () => {
     return (
         <div className="page-wrapper layout layout-auth">
-            <header className="header">Header</header>
             <main className="main">
                 <Outlet />
             </main>
-            <footer>Footer</footer>
         </div>
     );
 };
