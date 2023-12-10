2682d7a7edd8720294a30849097043c69e453fe8
realayna <100434027+realayna@users.noreply.github.com>
2023-12-10 20:18:31 +0100

    4th commit

--- src/pages/ContactPage/contactPage.tsx
+++ src/pages/ContactPage/contactPage.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react';
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
 
-
 export const ContactPage: FC = () => {
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -10,14 +9,14 @@ export const ContactPage: FC = () => {
     };
 
     return (
-        <div className="page page-home">
+        <div className="page page-contact">
             <Header />
             <main className="main">
-                <section className="section section-hero">
+                <section className="section section-contact">
                     <div className="container">
-                        <div className="row justify-content-center pb-75">
+                        <div className="row">
                             {/* First Box */}
-                            <div className="col-xl-4 col-lg-6 col-md-6">
+                            <div className="firstBox">
                                 <div className="contact-item">
                                     <div className="contact-info">
                                         <h3>Visit Us Anytime</h3>
@@ -27,52 +26,56 @@ export const ContactPage: FC = () => {
                             </div>
 
                             {/* Second Box */}
-                            <div className="col-xl-4 col-lg-6 col-md-6">
+                            <div className="secondBox">
                                 <div className="contact-item">
                                     <div className="contact-info">
                                         <h3>Send An Email</h3>
                                         <a href="mailto:hello@teli.com">support@medai.com</a>
-                                        <br />
                                     </div>
                                 </div>
                             </div>
 
                             {/* Third Box */}
-                            <div className="col-xl-4 col-lg-6 col-md-6">
+                            <div className="thirdBox">
                                 <div className="contact-item">
                                     <div className="contact-info">
                                         <h3>Call Center</h3>
-                                        <a href="tel:+44587154765">+48123456789</a>
+                                        <a href="tel:+48123456789">+48123456789</a>
                                     </div>
                                 </div>
                             </div>
+                        </div>
+                        <div className="contact-form-box">
 
-                            {/* Contact Form */}
-                            <div className="col-md-12 mt-4">
-                                <form className="contact-form" onSubmit={handleSubmit}>
-                                    <h3>Send Us A Message</h3>
-                                    <div className="form-group">
-                                        <input type="text" name="name" placeholder="Name*" required />
-                                    </div>
-                                    <div className="form-group">
-                                        <input type="email" name="email" placeholder="Email*" required />
-                                    </div>
-                                    <div className="form-group">
-                                        <input type="tel" name="phone" placeholder="Phone Number*" required />
-                                    </div>
-                                    <div className="form-group">
-                                        <input type="text" name="subject" placeholder="Subject*" required />
-                                    </div>
-                                    <div className="form-group">
-                                        <textarea name="message" placeholder="Your Messages.." rows={5} required></textarea>
-                                    </div>
-                                    <div className="form-group">
-                                        <button type="submit">Send Message</button>
-                                    </div>
-                                </form>
-                            </div>
+
+                            <form className="contact-form" onSubmit={handleSubmit}>
+                                <h3>Send Us A Message</h3>
+                                <div className="form-group">
+                                    <input type="text" name="name" placeholder="Name*" required />
+                                </div>
+                                <div className="form-group">
+                                    <input type="email" name="email" placeholder="Email*" required />
+                                </div>
+                                <div className="form-group">
+                                    <input type="tel" name="phone" placeholder="Phone Number*" required />
+                                </div>
+                                <div className="form-group">
+                                    <input type="text" name="subject" placeholder="Subject*" required />
+                                </div>
+                                <div className="form-group">
+                                    <textarea name="message" placeholder="Your Messages.." rows={5} required></textarea>
+                                </div>
+                                <div className="form-group">
+                                    <button type="submit">Send Message</button>
+                                </div>
+                            </form>
+
+
                         </div>
                     </div>
+
+
+                    {/* Contact Form */}
 
                 </section>
                 <Footer />
