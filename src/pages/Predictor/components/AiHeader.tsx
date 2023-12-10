2b0b64c9f0d3a0a6aec5358fd99ee74b82d4fc8c
Hezx13 <nikolaipozitif@gmail.com>
2023-12-10 18:57:25 +0100

    heart disease form prediction

---
+++ src/pages/Predictor/components/AiHeader.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+
+const AiHeader = () => {
+    return (
+        <div className="headerBox">
+            <h1 className="title">
+                Evaluate your health with AI!
+            </h1>
+
+        </div>
+    )
+};
+
+export default AiHeader;
